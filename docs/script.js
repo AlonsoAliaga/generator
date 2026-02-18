c32f0df470ed3e668e0b252e5ba104e81a8714a5
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2026-02-17 20:37:13 -0500

    Update index.html

--- docs/script.js
+++ docs/script.js
@@ -2248,7 +2248,7 @@ function loadChecking() {
  let link = atob("aHR0cHM6Ly9hbG9uc29hcGkuZGlzY2xvdWQuYXBwL2NoZWNraW5nP3NpdGU9PHNpdGU+JmtleT08a2V5PiZsb2NrPTxsb2NrPiZnZW49PGdlbj4=")
     .replace(/<site>/g,"generator").replace(/<key>/g,"KEY-A")
     .replace(/<gen>/g,`old`)
-    .replace(/<lock>/g,(typeof window.getRandomStyle == "undefined" || typeof window.adBlockEnabled == "undefined" || window.adBlockEnabled || myTimeout != undefined) ? "yes" : "no"  );
+    .replace(/<lock>/g,(typeof window.getRandomStyle == "undefined" || typeof adBlockEnabled == "undefined" || adBlockEnabled || myTimeout != undefined) ? "yes" : "no"  );
  let counter = document.getElementById("online-counter");
  if(counter) {
    $.ajax({
